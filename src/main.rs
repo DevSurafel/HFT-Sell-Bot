34bf4299a8db29ede3acbe62fb916063cb4dbb36
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-02-27 09:45:34 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ type HmacSha256 = Hmac<Sha256>;
 const API_KEY: &str = "bg_2b02e2a62b65685cee763cc916285ed3";
 const SECRET_KEY: &str = "c347ccb5f4d73d8928f3c3a54258707e3bf2013400c38003fd5192d61dbeccae";
 const PASSPHRASE: &str = "HFTSellNow";
-const TARGET_TOKEN: &str = "ZOOUSDT";
-const COIN_AMOUNT: &str = "10000"; // Adjust based on balance
+const TARGET_TOKEN: &str = "BTCUSDT";
+const COIN_AMOUNT: &str = "0.002"; // Adjust based on balance
 
 // Endpoint constants
 const API_BASE_URL: &str = "https://api.bitget.com";
