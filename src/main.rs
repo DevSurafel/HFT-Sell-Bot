e1d66eb0fcc89dbb99645eb423693bc2e911ccf9
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-04-16 16:08:18 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ type HmacSha256 = Hmac<Sha256>;
 const API_KEY: &str = "bg_2b02e2a62b65685cee763cc916285ed3";
 const SECRET_KEY: &str = "c347ccb5f4d73d8928f3c3a54258707e3bf2013400c38003fd5192d61dbeccae";
 const PASSPHRASE: &str = "HFTSellNow";
-const TARGET_TOKEN: &str = "PAWSUSDT";
+const TARGET_TOKEN: &str = "BTCUSDT";
 const COIN_AMOUNT: &str = "1020200"; // Adjust based on balance
 
 // Endpoint constants
