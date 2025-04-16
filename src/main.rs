dad4a39346f482944ab77938b08275a00cbffcba
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-04-16 16:03:13 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ const API_KEY: &str = "bg_2b02e2a62b65685cee763cc916285ed3";
 const SECRET_KEY: &str = "c347ccb5f4d73d8928f3c3a54258707e3bf2013400c38003fd5192d61dbeccae";
 const PASSPHRASE: &str = "HFTSellNow";
 const TARGET_TOKEN: &str = "PAWSUSDT";
-const COIN_AMOUNT: &str = "984000"; // Adjust based on balance
+const COIN_AMOUNT: &str = "1020200"; // Adjust based on balance
 
 // Endpoint constants
 const API_BASE_URL: &str = "https://api.bitget.com";
